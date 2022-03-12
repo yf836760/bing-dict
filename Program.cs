a9c0c653991d379e641b680d7cd6f3a7d689c393
yf836760 <yf836760@outlook.com>
2022-03-12 09:59:40 +0800

    Refactoring
    Add a package called Html Agility Pack to parse html easily

--- Program.cs
+++ Program.cs
@@ -1,23 +1,85 @@
-﻿using System.Text.RegularExpressions;
+﻿using HtmlAgilityPack;
+using System.Text;
 
-string word = "";
-foreach(string arg in args)
+Console.OutputEncoding = Encoding.UTF8; //使console可以正常输出音标
+var folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);//存放单词本的位置
+var writer = new StreamWriter($"{folder}{Path.DirectorySeparatorChar}words.txt", true);//append
+
+
+string? words = "";
+foreach (var word in args)
 {
-    word += arg + "+";
+    words += word;
 }
-string requestURI = $"https://cn.bing.com/dict/search?q=";
-string searchURI = requestURI + word;
-HttpClient httpClient = new HttpClient();
-httpClient.Timeout = TimeSpan.FromMinutes(500);
-HttpResponseMessage response = httpClient.GetAsync(searchURI).Result;
-HttpContent content = response.Content;
-string result = content.ReadAsStringAsync().Result;
-Match m = Regex.Match(result, @"<meta name=""description"".*?>{1}");
-string mValue = m.Value;
-bool isNetExplained = mValue.Contains("网络释义");
-if (!isNetExplained) return;
-int pos = mValue.IndexOf("，");
-string str = mValue.Substring(pos+1);
-int len = str.Length;
-str = str.Substring(0, len-4);
-Console.WriteLine(str);
+CheckWordsLength(words); //bing词典最多接收100个字符
+string bingDictURL = "https://cn.bing.com/dict/search?q=";
+var html = $@"{bingDictURL}{words}";
+
+
+HtmlWeb web = new HtmlWeb();
+var htmlDoc = web.Load(html);
+var htmlDocNode = htmlDoc.DocumentNode;
+
+
+//单词的匹配
+var node = htmlDocNode
+    .SelectNodes("//head/meta")
+    .Where(node => node.GetAttributeValue("name", "null") == "description")
+    .First();
+
+
+//单词情景
+if (node.GetAttributeValue("content", "null") == "词典")
+{
+    return;
+}
+else
+{
+    writer.WriteLine(words);
+    writer.Close();
+    string dict = node.GetAttributeValue("content", "解析错误");
+    int posOfComma = dict.IndexOf("，");
+    string outPut = dict.Substring(posOfComma + 1);
+    Console.WriteLine($"{outPut}");
+}
+
+
+void CheckWordsLength(string word)
+{
+    if (words?.Length > 100) 
+    {
+        Console.WriteLine("字符数量超过100");
+        return;
+    }
+}
+
+/*句子的匹配
+var node2 = htmlDocNode
+    .SelectSingleNode("//body/div/div/div/div")
+    .FirstChild;
+
+var node2Trans = node2.NextSibling.NextSibling;
+var node2TransChildnodes = node2Trans.ChildNodes;
+string output2 = "";
+foreach (var div in node2TransChildnodes)
+{
+    output2 += div.InnerHtml;
+}
+*/
+
+
+/*句子的测试
+Console.WriteLine(output2);
+Console.WriteLine(node2Trans.OuterHtml);
+
+Console.WriteLine(node2.GetAttributeValue("class", "null"));
+*/
+
+/*句子情景的输出
+if (node2.InnerHtml == "计算机翻译")
+{
+    失败啦 可以输出 但是和浏览器的翻译存在差异
+}
+*/
+
+
